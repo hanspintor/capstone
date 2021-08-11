a852ec72f09d53c89ed34419bca73f20df462fa5
hanspintor <hpintor@gbox.adnu.edu.ph>
2021-08-11 12:51:44 +0800

    UI Updates inside the folder

---
+++ UI Updates/loginscreen.dart
@@ -0,0 +1,162 @@
+import 'package:ProExpress/reg_landing_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget{
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+Widget _alertmessage(){
+  return Center(
+    child: Column(
+
+    ),
+  );
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(120),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          flexibleSpace: Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Image.asset(
+                "assets/PROExpress-logo.png",
+              height: 120,
+              width: 120,
+            ),
+          ),
+          //title: Text("PROExpress"),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              // Container(
+              //   margin: EdgeInsets.symmetric(vertical: 50),
+              //   child: Column(
+              //     children: [
+              //       Text(
+              //         "Sign In",
+              //         style: TextStyle(
+              //           fontSize: 30,
+              //         ),
+              //       ),
+              //       Divider(
+              //         color: Color(0xffFD3F40),
+              //         thickness: 5,
+              //         indent: 150,
+              //         endIndent: 150,
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              Container(
+                  margin: EdgeInsets.only(top: 100),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 50),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 50,),
+                      child: TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 15),
+                      //margin: EdgeInsets.only(top: 190),
+                      height: MediaQuery.of(context).size.width / 8,
+                      width: MediaQuery.of(context).size.width / 1.3,
+                      child: FlatButton(
+                        onPressed: () {},
+
+                        color: Color(0xffA82A2A),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 8),
+                      child: InkWell(
+                        onTap: () {
+                          showDialog(
+                              context: context, builder: (BuildContext context) => AlertDialog(
+                            content: (_alertmessage()),
+                          )
+                          );
+                        },
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            color: Color(0xffFD3F40),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10, top: 90),
+                      child: Divider(
+                        thickness: 5,
+                        color: Colors.black,
+                        indent: 52,
+                        endIndent: 52,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 15),
+                      height: MediaQuery.of(context).size.width / 8,
+                      width: MediaQuery.of(context).size.width / 1.8,
+                      child: FlatButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(builder: (context) => new RegLandingPage()));
+                        },
+
+                        color: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(
+                          "Create a New Account",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
---
+++ UI Updates/reg_landing_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'signup_customer.dart';
+import 'signup_courier.dart';
+
+class RegLandingPage extends StatefulWidget{
+  @override
+  _RegLandingPageState createState() => _RegLandingPageState();
+}
+
+class _RegLandingPageState extends State<RegLandingPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(120),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.white,
+            flexibleSpace: Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Image.asset(
+                "assets/PROExpress-logo.png",
+                height: 120,
+                width: 120,
+              ),
+            ),
+            //title: Text("PROExpress"),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      child: IconButton(icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      ),
+                        onPressed: (){
+                          Navigator.pop(context, false);
+                        },
+                        iconSize: 25,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 0),
+                      child: Text(
+                        "Choose a Registration Form",
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.all(25),
+                  height: MediaQuery.of(context).size.width / 2.2,
+                  width: MediaQuery.of(context).size.width / 1.1,
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(builder: (context) => new SignupCustomer()));
+                    },
+                    color: Color(0xffA82A2A),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "Customer",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25),
+                  height: MediaQuery.of(context).size.width / 2.2,
+                  width: MediaQuery.of(context).size.width / 1.1,
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(builder: (context) => new SignupCourier()));
+                    },
+                    color: Color(0xffA82A2A),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "Courier",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+        )
+    );
+  }
+}
