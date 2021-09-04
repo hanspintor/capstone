82e79613a1f29a4b8cc887c846213d90c5aa56fc
Anselmo Oduca <anoduca@gbox.adnu.edu.ph>
2021-09-04 21:35:03 +0800

    GOODS

--- proxpress/lib/UI/CustomerUI/dashboard_customer.dart
+++ proxpress/lib/UI/CustomerUI/dashboard_customer.dart
@@ -106,7 +106,7 @@ class _DashboardCustomerState extends State<DashboardCustomer> {
           count=1;
         }
         _sessionTimer?.cancel();
-        _sessionTimer = new Timer(Duration(seconds: duration), handleTimeOut);
+        _sessionTimer = new Timer(Duration(minutes: duration), handleTimeOut);
         _sessionTimerPrint?.cancel();
         _sessionTimerPrint = new Timer(Duration(minutes: duration), () {
           print("Session Expired");
