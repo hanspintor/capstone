f7081a0d79f9dec7d4a3e931eef03191eca725c8
Hans Pintor <hpintor@gbox.adnu.edu.ph>
2021-09-14 16:03:53 +0800

    added gps module in the customer side

--- proxpress/lib/UI/CustomerUI/delivery_status.dart
+++ proxpress/lib/UI/CustomerUI/delivery_status.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:lottie/lottie.dart';
 
 class DeliveryStatus extends StatefulWidget {
@@ -9,6 +10,10 @@ class DeliveryStatus extends StatefulWidget {
 
 class _DeliveryStatusState extends State<DeliveryStatus> {
   double rating = 0;
+  static final _initialCameraPosition = CameraPosition(
+      target: LatLng(13.621980880497976, 123.19477396693487),
+      zoom: 15
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -82,13 +87,26 @@ class _DeliveryStatusState extends State<DeliveryStatus> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              margin: EdgeInsets.only(right: 40),
-                child: Lottie.asset('assets/delivery.json'),
-              // child: Text('Delivered',
-              //   style: TextStyle(fontSize: 50),
-              // ),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Text("Ongoing Delivery",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
             ),
+            SizedBox(height: 10),
+              Container(
+                height: 400,
+                width: 400,
+                child: GoogleMap(
+                  myLocationButtonEnabled: false,
+                  zoomControlsEnabled: false,
+                  initialCameraPosition: _initialCameraPosition,
+                ),
+              ),
+            SizedBox(height: 10),
             ElevatedButton(
               child: Text(
                 'Send Feedback',
