5b15fab97bb64de872ac931f3e39b3dcd3d28db9
Hans Pintor <hpintor@gbox.adnu.edu.ph>
2021-09-23 11:57:46 +0800

    fixedt the labels and navigation for other pages

--- proxpress/lib/main.dart
+++ proxpress/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:proxpress/UI/CourierUI/courier_profile.dart';
 import 'package:proxpress/UI/CourierUI/pending_deliveries.dart';
